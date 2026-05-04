9cf961abb60ee9956d2ec6d5055045f5f9b3d423
ricardomartinezcuesta-hub <ricardomartinezcuesta@gmail.com>
2026-05-03 20:19:18 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,33 @@
+#esto es un comentario de una sola linea
+""""esto es un comentario de 
+varias lineas"""
+
+#inicializando variables
+nombre="nombre del estudiante"
+edad=15
+estado=True
+nota=5.0
+
+#mostrar el contenido de las variables print()
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
+#que tipo de datos contienen cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+
+#vamos a utilizar la funcion input para recoger datos por medio del teclado.
+nombre=input("¿como te llamas?")
+edad=input("¿que edad tienes?)")
+estado=("¿en que estado te encuentras?")
+nota=("¿cual es tu nota?")
+
+#para visualizar que guardamos en las variables anteriores
+print("hola,",nombre,"un gusto conocerte")
+print("tu edad es:",15)
+print("tu estado es:",False)
+print("tu nota es:",45)
